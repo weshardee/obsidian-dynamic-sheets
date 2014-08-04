5c41cc9469f468535f97f670190e294c6ee93547
Wes Hardee <weshardee@gmail.com>
2014-08-04 09:23:29 -0500

    add gulp task to clean build folder and copy sample files

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var rimraf = require('gulp-rimraf');
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', function(cb) {
+    // place code for your default task here
+
+    rimraf('./web');
+
+    gulp.src('./bower_components/dynamic_sheet_templates/devkit/**/*')
+        .pipe(gulp.dest('./web'))
+    ;
 });
